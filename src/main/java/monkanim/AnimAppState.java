e5a8971e144de9b9dac0a415f49c48acf5fab30f
Nehon <remy.bouquet@gmail.com>
2017-03-19 18:41:10 +0100

    comments for explanations

--- src/main/java/monkanim/AnimAppState.java
+++ src/main/java/monkanim/AnimAppState.java
@@ -51,54 +51,54 @@ public class AnimAppState extends BaseAppState {
         rig.addControl(manager);
         rig.addControl(skelControl);
 
-        //layers
+        //creating a layer that affects the left arm.
+        //Note that there is a default layer that affects the entire skeleton
         manager.addLayer("wave").withMask(SkeletonMask.fromBone(skeleton, "shoulder.L"));
 
         //state machine
+        //creating a state for each animation (optional of course but for the demo
         manager.createState("idle").forAnims("idle");
         manager.createState("walk").forAnims("walk");
         manager.createState("jog").forAnims("jog");
         manager.createState("kick").forAnims("kick");
         manager.createState("run").forAnims("run");
         manager.createState("wave").forAnims("wave");
+
+        //creating a wave anim on the wave layer
         manager.createState("waveLayer").forAnims("wave").onLayer("wave");
+
+        //creating a blended anim composed of the walk, jog and run anims. (will blend according to speed
         manager.createState("walk_jog_run").forAnims("walk", "jog", "run").withBlendSpace(blendSpace);
+        //creating a blended anim composed of walk and jog anims, but with a fied blend value of 0.5
         ((LinearBlendSpace) (manager.createState("walk_jog").forAnims("walk", "jog").getBlendSpace())).setValue(0.5f);
+        //Mixed blending between a previously defined state and an animation clip.
         manager.createState("walk_jog_nestedRun").forAnims("walk_jog", "run").withBlendSpace(blendSpace);
 
+        //bootstrap state
         manager.startWith("idle");
 
-//        //Non functional and non java 8 API..
-//        AnimState state = manager.getState(ANY_STATE);
-//        if(state != null) {
-//            AnimState targetState = manager.getState("idle");
-//            if(targetState != null) {
-//                InterruptingTransition it = new InterruptingTransition(targetState);
-//                it.setTrigger(new TransitionTrigger() {
-//                    @Override
-//                    public boolean evaluate() {
-//                        return currentState.equals("idle");
-//                    }
-//                });
-//                state.addTransition(it);
-//            }
-//        }
-
-        //Functional with java 8.
+        //defining transitions and events
+        //Any state will intrrupt to the idel state when the currentState local variable is "idle". Note that the condition can be enything
         manager.findState(ANY_STATE).interruptTo("idle").when(() -> currentState.equals("idle"));
         manager.findState(ANY_STATE).interruptTo("walk").when(() -> currentState.equals("walk"));
         manager.findState(ANY_STATE).interruptTo("jog").when(() -> currentState.equals("jog"));
         manager.findState(ANY_STATE).interruptTo("kick").when(() -> currentState.equals("kick"));
         manager.findState(ANY_STATE).interruptTo("run").when(() -> currentState.equals("run"));
         manager.findState(ANY_STATE).interruptTo("wave").when(() -> currentState.equals("wave"));
-        manager.findState("wave").transitionTo(ANY_STATE);
-        manager.findState(ANY_STATE).interruptTo("waveLayer").when(() -> currentState.equals("waveLayer"));
-        manager.findState("waveLayer").transitionTo(ANY_STATE);
         manager.findState(ANY_STATE).interruptTo("walk_jog_run").when(() -> currentState.equals("walk_jog_run"));
         manager.findState(ANY_STATE).interruptTo("walk_jog").when(() -> currentState.equals("walk_jog"));
         manager.findState(ANY_STATE).interruptTo("walk_jog_nestedRun").when(() -> currentState.equals("walk_jog_nestedRun"));
 
-        //Chain
+        //Special case, when the wave state ends, it will return to "anystate", in that case the state from which wave was triggered
+        //Foexample if you go from walk to wave, when wave ends, it will transition bac to walk.
+        manager.findState("wave").transitionTo(ANY_STATE);
+
+        manager.findState(ANY_STATE).interruptTo("waveLayer").when(() -> currentState.equals("waveLayer"));
+        //Here again a special case if a layered state transition back to ANY_STATE, if will just fade out to the previous layer.
+        manager.findState("waveLayer").transitionTo(ANY_STATE);
+
+
+        //A chain of states that will go on while currentState is "anim_chain"
         manager.findState(ANY_STATE).interruptTo("walk").when(() -> currentState.equals("anim_chain"));
         manager.findState("walk").transitionTo("jog").when(() -> currentState.equals("anim_chain"));
         manager.findState("jog").transitionTo("run").when(() -> currentState.equals("anim_chain"));
